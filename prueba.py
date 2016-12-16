a9a28955fa95c8603641cb9e1f4134fdc4bd8900
Bruno <elbuen@brus.mx>
2016-12-15 20:06:49 -0800

    Nueva prueba de primalidad, y ya escribe a archivo 'primos.txt' uno por linea

--- prueba.py
+++ prueba.py
@@ -1,40 +1,53 @@
 #!/usr/bin/env python
+# pylint: disable-msg=C0103
 from __future__ import print_function
-lista_probados = []#Lista de los primos que ya sabemos que son primos
-lista_por_probar = [2,3,5,7]#Lista de primos que ya sabemos primos de un digito para generar la lista al vuelo de los siguientes 
+with open('primos.txt', 'w') as f1:
+#Lista de primos que ya sabemos primos de un digito para generar la lista al vuelo de los siguientes
+    lista_por_probar = [2, 3, 5, 7]
+    def is_prime_number(n):
+        """Returns True if n is prime."""
+        if n == 2:
+            return True
+        if n == 3:
+            return True
+        if n % 2 == 0:
+            return False
+        if n % 3 == 0:
+            return False
 
-#TODO sustituir prueba de primalidad con una de verdad y rapida
-def is_prime_number(x):
-    if x >= 2:
-        for y in range(2,x):
-            if not ( x % y ):
+        i = 5
+        w = 2
+
+        while i * i <= n:
+            if n % i == 0:
                 return False
-    else:
-	return False
-    return True
 
-print ("Empieza el ciclo")
-primo = lista_por_probar[0]
-print(str(primo) + ', ', end="")
-while primo != None and lista_por_probar:
-    for num in range(1,9):
-        aux = int(str(num)+str(primo))
-        if is_prime_number(int(aux)):
-            lista_por_probar.append(aux)
-    lista_probados.append(primo)
-    lista_por_probar.remove(primo)
-    if not lista_por_probar:
-        primo = None
-    else:
-        primo = lista_por_probar[0]
-        print(str(aux) + ', ', end="")
-print ("\nSe cierra el ciclo", end="\n")
+            i += w
+            w = 6 - w
 
-print ("La lista de primos probados contiene " + str(len(lista_probados)) + " elemento(s)")
-print("\nCOMIENZA")
-for elem in lista_probados:
-    print (str(elem) + "," , end="") 
-print("\nFIN")
-print ("La lista de primos por probar contiene " + str(len(lista_por_probar)) + " elemento(s)")
-for elem in lista_por_probar:
-    print (str(elem) + "," , end="") 
+        return True
+    indice = 0
+    cant_primos = 1
+    #Optimizacion para no recorrer el arreglo  siempre para contar los elementos
+    tam_arreglo = len(lista_por_probar)
+    print ("Empieza el ciclo", file=f1)
+    primo = lista_por_probar[indice]
+    print(str(primo), end="\n", file=f1)
+    while indice <= tam_arreglo:
+        for num in range(1, 9):
+            aux = int(str(num)+str(primo))
+            if is_prime_number(int(aux)):
+                lista_por_probar.append(aux)
+                print(str(aux), end="\n", file=f1)
+                cant_primos = cant_primos +1
+                tam_arreglo = tam_arreglo + 1
+                if tam_arreglo % 10 == 0:
+                    print("Llevas "+str(tam_arreglo)+ " primos")
+        indice = indice + 1
+        if indice <= tam_arreglo - 1:
+            primo = lista_por_probar[indice]
+            print(str(primo), end="\n", file=f1)
+    print ("Se cierra el ciclo", end="\n", file=f1)
+    print ("La lista de primos por probar contiene " + str(len(lista_por_probar)) + " elemento(s)")
+    for elem in lista_por_probar:
+        print (str(elem)+ ",", end="")
